e2def46188bec3305341963ba8262b0f34385ab9
Craig Zeise <craig.zeise@gmail.com>
2017-11-19 16:49:15 -0600

    Add stack navigation to entry detail.

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Platform, StatusBar } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { TabNavigator } from 'react-navigation';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import { Constants } from 'expo';
 
@@ -10,6 +10,7 @@ import AddEntry from './components/AddEntry';
 import reducer from './reducers/index';
 import History from './components/History';
 import { purple, white } from './utils/colors';
+import EntryDetail from './components/EntryDetail';
 
 function UdaciStatusBar({ backgroundColor, ...props }) {
   return (
@@ -56,13 +57,29 @@ const Tabs = TabNavigator({
   }
 });
 
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+
+  EntryDetail: {
+    screen: EntryDetail,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple
+      }
+    }
+  }
+});
+
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
           <UdaciStatusBar backgroundColor={purple} barStyle='light-content' />
-          <Tabs />
+          <MainNavigator />
         </View>
       </Provider>
     );
